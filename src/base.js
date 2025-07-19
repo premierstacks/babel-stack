83a85e30b7a6cb61d308217bf2aa234ce1d1b6c9
Tomáš Chochola <chocholatom1997@gmail.com>
2025-07-20 00:55:44 +0200

    Return back to CC-BY-ND-4.0

--- src/base.js
+++ src/base.js
@@ -3,9 +3,9 @@
  * @author Tomáš Chochola <chocholatom1997@gmail.com>
  * @copyright © 2025 Tomáš Chochola <chocholatom1997@gmail.com>
  *
- * @license Apache-2.0
+ * @license CC-BY-ND-4.0
  *
- * @see {@link http://www.apache.org/licenses/LICENSE-2.0} License
+ * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
  * @see {@link https://github.com/tomchochola} GitHub Personal
  * @see {@link https://github.com/premierstacks} GitHub Organization
  * @see {@link https://github.com/sponsors/tomchochola} GitHub Sponsors
@@ -20,7 +20,6 @@ export function createBabelConfigBase() {
       [
         '@babel/preset-env',
         {
-          debug: process.env.NODE_ENV === 'development',
           bugfixes: true,
           modules: false,
           useBuiltIns: 'entry',
