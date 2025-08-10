b63f760f33ab8b424d67830ba25139e684c60fef
Tomáš Chochola <chocholatom1997@gmail.com>
2025-08-10 23:07:46 +0200

    Add license doc blocks

--- src/builder.js
+++ src/builder.js
@@ -1,3 +1,16 @@
+/**
+ * @file
+ * @author Tomáš Chochola <chocholatom1997@gmail.com>
+ * @copyright © 2025 Tomáš Chochola <chocholatom1997@gmail.com>
+ *
+ * @license CC-BY-ND-4.0
+ *
+ * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
+ * @see {@link https://github.com/tomchochola} GitHub Personal
+ * @see {@link https://github.com/premierstacks} GitHub Organization
+ * @see {@link https://github.com/sponsors/tomchochola} GitHub Sponsors
+ */
+
 import { createBabelConfig, withPresetEnv } from './base.js';
 import { withPluginReactCompiler, withPluginStylex, withPresetReact } from './react.js';
 import { withPresetTypescript } from './typescript.js';
@@ -67,7 +80,7 @@ export class BabelStack {
     });
   }
 
-  static get presets() {
+  static get Presets() {
     return presets;
   }
 }
